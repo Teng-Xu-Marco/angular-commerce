89ddc599291b1d915d0677beef38e2c66a9d0b0b
Teng-Xu-Marco <shuateng@163.com>
2024-08-28 21:27:33 -0700

    increase, decrease and remove product from shopping cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 export const routes: Routes = [
     {path: 'category/:id', component: ProductListComponent},
@@ -8,6 +9,7 @@ export const routes: Routes = [
     {path: 'products/:id', component: ProductDetailsComponent},
     {path: 'products', component: ProductListComponent},
     {path: 'search/:keyword', component: ProductListComponent},
+    {path: 'cart-details', component: CartDetailsComponent},
     {path: '', redirectTo: '/products', pathMatch: 'full'},
     {path: '**', redirectTo: '/products', pathMatch: 'full'}
 ];
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,45 +3,65 @@ import { CartItem } from '../common/cart-item';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   cartItems: CartItem[] = [];
 
   // can be subscribed
-  totalPrice: Subject<number> = new Subject<number>;
-  totalQuantity: Subject<number> = new Subject<number>;;
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
 
-  constructor() { }
+  constructor() {}
 
   addToCart(theCartItem: CartItem) {
     let existedCartItem: CartItem | undefined;
-    let curTotalPrice: number = 0.00
-    let curTotalQuantity: number = 0;
 
     if (this.cartItems.length > 0) {
-      for(let tmpCartItem of this.cartItems) {
-        curTotalPrice += tmpCartItem.unitPrice * tmpCartItem.quantity;
-        curTotalQuantity += tmpCartItem.quantity;
-  
-        if (tmpCartItem.id == theCartItem.id) {
-          existedCartItem = tmpCartItem;
-        }
-      }
+      // find the 1st matched item in the array
+      existedCartItem = this.cartItems.find(
+        (item) => item.id === theCartItem.id
+      );
     }
 
     if (existedCartItem != undefined) {
       existedCartItem.quantity += 1;
     } else {
-      this.cartItems.push(theCartItem)
+      this.cartItems.push(theCartItem);
     }
 
-    curTotalPrice += theCartItem.unitPrice;
-    curTotalQuantity += 1;
+    this.computeTotal();
+  }
+
+  computeTotal() {
+    let curTotalPrice: number = 0;
+    let curTotalQuantity: number = 0;
+
+    for (let tmpCartItem of this.cartItems) {
+      curTotalPrice += tmpCartItem.unitPrice * tmpCartItem.quantity;
+      curTotalQuantity += tmpCartItem.quantity;
+    }
 
     // push/publish the value to all the subscribers
     this.totalPrice.next(curTotalPrice);
     this.totalQuantity.next(curTotalQuantity);
+  }
+
+  decreaseQuantity(theCartitem: CartItem) {
+    theCartitem.quantity -= 1;
+  
+    if (theCartitem.quantity === 0) {
+      this.removeFromCart(theCartitem);
+    }else{
+      this.computeTotal();
+    }
+  }
+
+  removeFromCart(theCartitem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartitem.id);
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+    }
+    this.computeTotal();
   }
 }
